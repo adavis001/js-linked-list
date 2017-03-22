ab9671ce777346bfd25a0f8d706b5547764e19b9
Alexander Davis <akonidavis@gmail.com>
2017-03-21 14:11:42 -1000

    completed

--- linkedList.js
+++ linkedList.js
@@ -29,21 +29,47 @@ function linkedListGenerator(){
 	}
 	function get(number) {
 		var currentNode = head;
+
 		for(i = 0; i<number; i++){
 			if (currentNode.next === null) {
 				return false;
 			}
 			currentNode = currentNode.next;
 		}
-		console.log(currentNode);
 		return currentNode;
 	}
 	function remove(number) {
-
+		var prevNode = get(number - 1);
+		var postNode = get(number + 1);
+		if(postNode=== false){
+			prevNode.next = null;
+			tail = prevNode;
+		} if(number === 0){
+			head = postNode;
+		} if(get(number) === false){
+			return false;
+		}
+		prevNode.next = postNode;
 	}
-	function insert(value, number) {
+	function insert(value,number) {
+      var insertedNode = {
+        value: value,
+        next: null
+      };
+      var backNode = get(number - 1);
+      var firstNode = get(number + 1);
+      if(number === 0 ) {
+        insertedNode.next = head;
+        head = insertedNode;
+      }
+      else if(number < 0 || get(number) === false) {
+        return false;
 
-	}
+     } else {
+        insertedNode.next  = get(number);
+        backNode.next = insertedNode;
+      }
+    }
 
 	return {
 		getHead: getHead,
